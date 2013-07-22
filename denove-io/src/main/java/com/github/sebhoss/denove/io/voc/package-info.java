18a6feca4a5a465a8f698903e3ac2c75290a5522
Sebastian Hoß <mail@shoss.de>
2013-07-22 12:58:38 +0200

    Added Langenscheid vocabulary trainer
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ denove-io/src/main/java/com/github/sebhoss/denove/io/voc/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Website at http://www.vokabeln.de/
+ * Different file formats (v1 to v6):
+ *   http://www.vokabeln.de/v1/download.htm
+ *   http://www.vokabeln.de/v2/download.htm
+ *   http://www.vokabeln.de/v3/download.htm
+ *   http://www.vokabeln.de/v4/download.htm
+ *   http://www.vokabeln.de/v5/download.htm
+ *   http://www.vokabeln.de/v6/download.htm
+ */
+package com.github.sebhoss.denove.io.voc;
