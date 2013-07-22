52c79239443832affafc0f5556bbc9560d04f07b
Sebastian Hoß <mail@shoss.de>
2013-07-22 21:38:18 +0200

    Created common interface for XML elements
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ denove-io/src/main/java/com/github/sebhoss/denove/io/common/Element.java
@@ -0,0 +1,13 @@
+package com.github.sebhoss.denove.io.common;
+
+/**
+ * Interface for XML elements.
+ */
+public interface Element {
+
+    /**
+     * @return Identifier (or tag name) of this element.
+     */
+    String getIdentifier();
+
+}
