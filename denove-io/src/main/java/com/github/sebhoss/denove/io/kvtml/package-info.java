268fb17395b284eebc4d82a5f123a5fe9984e772
Sebastian Hoß <mail@shoss.de>
2013-07-23 03:45:33 +0200

    Merged KVTML2 with KVTML
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- denove-io/src/main/java/com/github/sebhoss/denove/io/kvtml/package-info.java
+++ denove-io/src/main/java/com/github/sebhoss/denove/io/kvtml/package-info.java
@@ -1,4 +1,8 @@
 /**
+ * KVTML file format is used by several programs, mostly in the KDE universe.
  * 
+ * Version 2 is documented at:
+ *  http://techbase.kde.org/Projects/Edu/kvtml2
+ *  http://edu.kde.org/kvtml/kvtml2.xsd
  */
 package com.github.sebhoss.denove.io.kvtml;
--- denove-io/src/main/java/com/github/sebhoss/denove/io/kvtml2/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Schema at
- *  http://techbase.kde.org/Projects/Edu/kvtml2
- *  http://edu.kde.org/kvtml/kvtml2.xsd
- */
-package com.github.sebhoss.denove.io.kvtml2;
