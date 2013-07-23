a69425f9be5af09d682fc8ed275d6b8413da6fd7
Sebastian Hoß <mail@shoss.de>
2013-07-23 03:52:28 +0200

    Added enum of supported file formats
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ denove-io/src/main/java/com/github/sebhoss/denove/io/common/FileFormats.java
@@ -0,0 +1,17 @@
+package com.github.sebhoss.denove.io.common;
+
+/**
+ * Enumeration of supported file formats.
+ */
+public enum FileFormats {
+
+    /** Format used by Anki */
+    ANKI,
+
+    /** Format used by several KDE programs */
+    KVTML,
+
+    /** Format used by Memorize-Words */
+    MEMORIZE_WORDS;
+
+}
